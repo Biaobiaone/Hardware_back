97846cb6cc1961f4f28bb21e50d425df2e47196d
Biaobiaone <551738030@qq.com>
2026-03-10 09:19:55 +0800

    项目基础配置

---
+++ src/main/java/com/hardware/hardware_back/HardwareBackApplication.java
@@ -0,0 +1,13 @@
+package com.hardware.hardware_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HardwareBackApplication.class, args);
+    }
+
+}
